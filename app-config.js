be62a7994868562ed98c27d4a08312df7ce15a64
phandungmta <dungcnttk51mta@gmail.com>
2026-06-13 09:15:22 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -1,3 +1,3 @@
 window.APP_REMOTE_CONFIG = {
-  bridgeUrl: 'https://script.google.com/macros/s/AKfycbzaluFbu_qqalxfXIERdv7SsMQMU9QINAh5a4uzaeOOdW8i01bAlthdk9z7bWjUvcCO/exec'
+  bridgeUrl: 'https://script.google.com/macros/s/AKfycbySZeitDAPXKM-z5HPgS3nL0a28rDla8547j0FN296ZSzGeTy4GHVfMTCU6-Vp7Rlsy3w/exec'
 };
